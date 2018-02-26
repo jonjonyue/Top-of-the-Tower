737173e82c28975ba5eb8e964b98684c138e22ae
Jonathan Yue <yuejonathan@wustl.edu>
2018-02-26 13:12:18 -0600

    gitignore
    added a .gitignore and removed unnecessary stuff

---
+++ Top of the Tower/Assets/Scripts/character.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class character : MonoBehaviour
+{
+	// Enums
+	public enum charType {player, enemy};
+
+	// Informational
+	public string name;
+
+	// General Stats
+	public int health;
+	public int defense;
+	public int speed;
+
+	// Code variables
+	public bool isAlive = 0;
+
+	// Use this for initialization
+	void Start ()
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+	
+	}
+}
+
