9cbd70dd1b11cf9c0c60af09050df86b1b4185f8
Jonathan Yue <yuejonathan@wustl.edu>
2018-02-26 13:05:31 -0600

    quick setup
    setup of general classes for tott

---
+++ Top of the Tower/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : character {
+
+	/*
+	 * Variables in Character
+	 * 
+	// Enums
+	public enum charType {player, enemy};
+
+ 	// Informational
+	public string name;
+
+ 	// General Stats
+	public int health;
+	public int defense;
+	public int speed;
+
+ 	// Code variables
+	public bool isAlive = 0;
+	*/
+
+	// Player Specific stats
+	public int mana;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
