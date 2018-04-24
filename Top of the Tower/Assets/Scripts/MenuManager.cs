d6f0172eac7fb4cbe0ac353320134fe652798f1e
Jonathan Yue <yuejonathan@wustl.edu>
2018-04-23 22:32:42 -0500

    adding story

--- Top of the Tower/Assets/Scripts/MenuManager.cs
+++ Top of the Tower/Assets/Scripts/MenuManager.cs
@@ -9,16 +9,23 @@ public class MenuManager : MonoBehaviour {
     [Header("UI Objects")]
     public GameObject menuCanvas;
     public GameObject tutorialCanvas;
+    public GameObject storyCanvas;
     public Image tutorialImage;
+    public Image storyImage;
     public Sprite[] sprites;
+    public Sprite[] story;
 
     private bool inTutorial = false;
+    private bool inStory = false;
+
     private int spritesCounter = 0;
+    private int storyCounter = 0;
 
 	// Use this for initialization
 	void Start () {
         menuCanvas.SetActive(true);
         tutorialCanvas.SetActive(false);
+        storyCanvas.SetActive(false);
 	}
 	
 	// Update is called once per frame
@@ -27,13 +34,33 @@ public class MenuManager : MonoBehaviour {
             if (Input.GetKeyDown(KeyCode.Space)) {
                 spritesCounter++;
 
-                if (spritesCounter >= sprites.Length)
-                    play();
-                else
+                if (spritesCounter >= sprites.Length){
+                    storyCanvas.SetActive(true);
+                    tutorialCanvas.SetActive(false);
+                    inTutorial = false;
+                    inStory = true;
+                } else
                     tutorialImage.sprite = sprites[spritesCounter];
             }
         }
+        if(inStory) {
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetMouseButtonDown(0))
+            {
+                storyCounter++;
+
+                if (storyCounter >= story.Length)
+                    play();
+                else
+                    storyImage.sprite = story[storyCounter];
+            }
+        }
 	}
+
+    public void startStory() {
+        inStory = true;
+        menuCanvas.SetActive(false);
+        storyCanvas.SetActive(true);
+    }
 
     public void play() {
         SceneManager.LoadSceneAsync(1);
